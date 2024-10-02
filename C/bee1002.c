801afc7c363014057998bf9c8a29cc857274eda9
Guilherme Aquino <72028922+aquino-gui@users.noreply.github.com>
2024-10-02 15:26:09 -0300

    Add files via upload

---
+++ C/bee1002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  double pi = 3.14159;
+  double raio, area;
+  scanf("%lf", &raio);
+
+  area = pi * pow(raio, 2);
+
+  printf("A=%.4lf\n", area);
+  return 0;
+}
